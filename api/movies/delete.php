d81b6a38f7bd306295f46ff9ddef68bb565bf4d7
npgarima <thegarimaweb@gmail.com>
2023-04-23 13:56:05 +0545

    Updated codes

--- api/movies/delete.php
+++ api/movies/delete.php
@@ -1,7 +1,13 @@
 <?php
 header("Content-Type: application/json");
 if($_SERVER['REQUEST_METHOD']=="DELETE"){
-
+    //echo $_GET['id'];
+    // gali khana manxaina vane echo nagarne 
+  $response=array(
+    'id'=>$_GET['id'],
+    'success'=>true
+  );
+  echo json_encode($response);
 
 }
 else{
--- api/movies/post.php
+++ api/movies/post.php
@@ -1,7 +1,20 @@
 <?php
 //post= CREATE
 if($_SERVER['REQUEST_METHOD']=="POST"){
+//we never send query on post method
 
+ $json_data= file_get_contents("php://input");
+ 
+ //print_r($json_data);
+
+ $request=json_decode($json_data);
+
+ $response=array(
+    'id'=>$request->id,
+    'success'=>true
+  );
+
+  echo json_encode($response);
 
 }
 else{
